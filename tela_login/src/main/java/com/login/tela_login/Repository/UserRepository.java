fc887bbb7879574ee55fc8d50a65c344ee71a970
Alefe Gustavo <alefedonato777@gmail.com>
2023-03-13 09:51:45 -0300

    Pastas de classes criandas e arquivos iniciais

---
+++ tela_login/src/main/java/com/login/tela_login/Repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.login.tela_login.Repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository{
+    
+}
